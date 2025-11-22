8f745f5d89689a71f520b95eb5d815b110673fc2
shannon petkau <spetkau2@rrc.ca>
2025-11-22 13:25:05 -0600

    feat: Refactored episodeController.ts

--- src/api/v1/controllers/episodeController.ts
+++ src/api/v1/controllers/episodeController.ts
@@ -1,5 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 import { HTTP_STATUS } from "../../../constants/httpConstants";
+import * as episodeService from "../services/episodeService";
+import { Episode } from "../models/episodeModel";
 
 /**
  * Manages requests and reponses to retrieve all Episodes
@@ -13,8 +15,10 @@ export const getAllEpisodes = async (
     next: NextFunction
 ): Promise<void> => {
     try {
+        const episodes: Episode[] = await episodeService.getAllEpisodes();
         res.status(HTTP_STATUS.OK).json({
             message: "Episodes retrieved successfully",
+            data: episodes,
         });
     } catch (error: unknown) {
         next(error);
@@ -33,8 +37,27 @@ export const getOneEpisode = async (
     next: NextFunction
 ): Promise<void> => {
     try {
+        const id: string = req.params.id;
+        
+        if (!id || id.trim() === "") {
+            res.status(HTTP_STATUS.BAD_REQUEST).json({
+                message: "Episode ID is required."
+            });
+            return;
+        }
+        
+        const episode: Episode = await episodeService.getOneEpisode(id);
+        
+        if (!episode) {
+            res.status(HTTP_STATUS.NOT_FOUND).json({
+                message: "Episode not found."
+            });
+            return;
+        }
+        
         res.status(HTTP_STATUS.OK).json({
             message: "Episode retrieved successfully",
+            data: episode,
         });
     } catch (error: unknown) {
         next(error);
@@ -53,9 +76,40 @@ export const createEpisode = async (
     next: NextFunction
 ): Promise<void> => {
     try {
+        const requiredFields: (keyof Episode)[] = [
+            'id',
+            'title',
+            'season',
+            'episode',
+            'director',
+            'writers',
+        ];
+        
+        const missingFields = requiredFields.filter(field => !(field in req.body));
+        
+        if (missingFields.length > 0) {
+            res.status(HTTP_STATUS.BAD_REQUEST).json({
+                message: "Missing required parameter."
+            });
+            return;
+        }
+        
+        const { id, title, season, episode, director, writers } = req.body;
+        
+        const newEpisode: Episode = await episodeService.createEpisode({
+            id,
+            title,
+            season,
+            episode,
+            director,
+            writers,
+        });
+
         res.status(HTTP_STATUS.CREATED).json({
             message: "Episode created successfully",
+            data: newEpisode,
         });
+    
     } catch (error: unknown) {
         next(error);
     }
@@ -73,9 +127,37 @@ export const updateEpisode = async (
     next: NextFunction
 ): Promise<void> => {
     try {
+        const id: string = req.params.id;
+        
+        const { title, writers } = req.body;
+        
+        if (!id || id.trim() === "") {
+            res.status(HTTP_STATUS.BAD_REQUEST).json({
+                message: "Episode ID is required.",
+            });
+            return;
+        }
+        
+        const updatedEpisode: Episode = await episodeService.updateEpisode(id, { title, writers });
+        
+        if (!title) {
+            res.status(HTTP_STATUS.BAD_REQUEST).json({
+                message: "Title string is empty.",
+            });
+            return;
+        }
+        
+        if (!writers) {
+            res.status(HTTP_STATUS.BAD_REQUEST).json({
+                message: "Writers array is empty.",
+            });
+            return;
+        }
         res.status(HTTP_STATUS.OK).json({
             message: "Episode updated successfully",
+            data: updatedEpisode,
         });
+    
     } catch (error: unknown) {
         next(error);
     }
@@ -93,9 +175,28 @@ export const deleteEpisode = async (
     next: NextFunction
 ): Promise<void> => {
     try {
+        const id: string = req.params.id;
+        
+        if (!id || id.trim() === "") {
+            res.status(HTTP_STATUS.BAD_REQUEST).json({
+                message: "Episode ID is required.",
+            });
+            return;
+        }
+        
+        const episode: Episode = await episodeService.getOneEpisode(id);
+        
+        if (!episode) {
+            res.status(HTTP_STATUS.NOT_FOUND).json({
+                message: "Episode not found.",
+            });
+            return;
+        }
+
         res.status(HTTP_STATUS.OK).json({
             message: "Episode successfully deleted",
         });
+        
     } catch (error: unknown) {
         next(error);
     }
