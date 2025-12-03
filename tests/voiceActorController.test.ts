b553320fe1d2e34ee89db43292fb4450ee3b58b8
shannon petkau <spetkau2@rrc.ca>
2025-12-03 14:27:46 -0600

    feat: Refactored voiceActorController.test.ts.

--- tests/voiceActorController.test.ts
+++ tests/voiceActorController.test.ts
@@ -8,6 +8,14 @@ import { sampleVoiceActors as mockVoiceActors } from "../src/data/voiceActors";
 
 jest.mock("../src/api/v1/services/voiceActorService");
 
+jest.mock('../src/api/v1/repositories/firestoreRepository', () => ({
+    getDocuments: jest.fn(),
+    getDocumentById: jest.fn(),
+    createDocument: jest.fn(),
+    updateDocument: jest.fn(),
+    deleteDocument: jest.fn(),
+}));
+
 describe("Voice Actor Controller", () => {
     let mockReq: Partial<Request>;
     let mockRes: Partial<Response>;
