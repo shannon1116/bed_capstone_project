5ea3210b8089f31e46b9add4c967a1956c230e07
shannon petkau <spetkau2@rrc.ca>
2025-12-03 11:16:41 -0600

    feat: Added tests to customClaims.test.ts to test setCustomClaims and refactored setCustomClaims to work with tests.

--- src/api/v1/controllers/adminController.ts
+++ src/api/v1/controllers/adminController.ts
@@ -1,32 +1,34 @@
-// External library imports
+// External imports
 import { Request, Response, NextFunction } from "express";
 
-// Internal module imports
+// Internal imports
 import { auth } from "../../../../config/firebaseConfig";
 import { successResponse } from "../models/responseModel";
-
-const OK: number = 200;
+import { HTTP_STATUS } from "../../../constants/httpConstants";
 
 /**
- * Handles setting custom claims for a user.
- * @param {Request} req - The request object.
- * @param {Response} res - The response object.
- * @param {NextFunction} next - The next middleware function.
- * @returns {Promise<void>}
+ * Handles setting roles (custom claims in Firebase) for a user
+ * @param req - The request object
+ * @param res - The response object
+ * @param next - The next middleware function
  */
 export const setCustomClaims = async (
-    req: Request,
-    res: Response,
-    next: NextFunction
+  req: Request,
+  res: Response,
+  next: NextFunction
 ): Promise<void> => {
-    const { uid, claims } = req.body;
+  try {
+    const { uid, roles } = req.body;
 
-    try {
-        await auth.setCustomUserClaims(uid, claims);
-        res.status(OK).json(
-            successResponse({}, `Custom claims set for user: ${uid}`)
-        );
-    } catch (error) {
-        next(error);
-    }
+    const user = await auth.getUser(uid);
+    const existingClaims = user.customClaims || {};
+
+    await auth.setCustomUserClaims(uid, { ...existingClaims, ...roles });
+
+    res.status(HTTP_STATUS.OK).json(
+      successResponse({}, `Custom claims (roles) set for user: ${uid}`)
+    );
+  } catch (error: unknown) {
+    next(error);
+  }
 };
